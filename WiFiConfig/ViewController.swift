3ab6601800a92c60b73202e7e3b6986fd8b05ac7
Patrick Maltagliati <patmalt@gmail.com>
2018-01-05 15:48:11 -0600

    Set the WiFi config on button tap

--- WiFiConfig/ViewController.swift
+++ WiFiConfig/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import NetworkExtension
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ssidTextField: UITextField?
+    @IBOutlet weak var passwordTextField: UITextField?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        ssidTextField?.delegate = self
+        passwordTextField?.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func configureButtonTapped() {
+        guard let ssid = ssidTextField?.text, let password = passwordTextField?.text else {
+            print("SSID or password missing")
+            return
+        }
+        if #available(iOS 11.0, *) {
+            let configuration = NEHotspotConfiguration(ssid: ssid, passphrase: password, isWEP: false)
+            NEHotspotConfigurationManager.shared.apply(configuration) { (error) in
+                if let error = error {
+                    print(error)
+                } else {
+                    print("Successfully joined the network")
+                }
+            }
+        } else {
+            print("WiFi Config only allowed on iOS 11")
+        }
     }
+}
 
-
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
